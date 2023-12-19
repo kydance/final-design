Fix bug: cost too long time when updating gradient in runtime.

utils/compress.py
@@ -1,3 +1,4 @@
+import time, copy
 import torch, math
 
 # TODO reconstruction functions with class
@@ -5,7 +6,7 @@ import torch, math
 def importance_abs(tensor):
     return tensor.abs()
 
-def update(grad, indices, name, v):
+def _update(grad, indices, name, v):
     '''Update grad with gradient accumulate.
         return: the gradient of updated.
     '''
@@ -13,10 +14,13 @@ def update(grad, indices, name, v):
     v_vec = v[name].view(-1)
     v_vec += grad
     A = torch.zeros_like(v_vec).to(grad.device)
-    indicesList = indices.tolist()
-    for i in indicesList:
-        A[i] = v_vec[i]
-        v_vec[i] = 0
+
+    A = copy.deepcopy(v_vec)
+    A.index_fill_(0, indices, 0)
+    A = -A
+    A += v_vec
+    v_vec.index_fill_(0, indices, 0)
+
     v[name] = v_vec.view(v[name].shape)
     return A.view(v[name].shape)
 
@@ -31,9 +35,10 @@ def sparsify(model, compress_cr, v, importance_fn=importance_abs):
 
             # indices = mask.nonzero().view(-1)
             # indices = indices[:numReserved]
-            indices = mask.nonzero().view(-1)[:numReserved]
+            # indices = mask.nonzero().view(-1)[:numReserved]
+            indices = torch.nonzero(mask).view(-1)[:numReserved]
 
-            param.grad.data = update(grad, indices, name, v)
+            param.grad.data = _update(grad, indices, name, v)
         else:
             raise "grad must be tensor!" # type: ignore
 
@@ -52,6 +57,6 @@ def sparsify_local(model, compress_cr, v):
             num_indices = indices.numel()
             indices = indices[:numReserved]
             values = grad[indices]
-            param.grad.data = update(grad, indices, name, v)
+            param.grad.data = _update(grad, indices, name, v)
         else:
             raise "grad must be tensor!" # type: ignore