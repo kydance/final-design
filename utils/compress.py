17a9daa66f4ba9cbff4cc1e0b98b801f7ef1da4b
lutianen <tianen.xd@gmail.com>
2024-01-16 11:18:41 +0800

    Adjust code structure for compress.py

--- utils/compress.py
+++ utils/compress.py
@@ -6,7 +6,7 @@ from scipy.spatial import distance
 
 def sparsify(model, compress_cr, v, dist_type: str="abs"):
     if dist_type == "base_line":
-        return
+        return _base_line()
 
     for name, param in model.named_parameters():
         if param.grad is None: # 由于只使用了 G，因此需要派出 D 相关的梯度
@@ -24,74 +24,13 @@ def sparsify(model, compress_cr, v, dist_type: str="abs"):
         else:
             raise Exception("grad must be tensor!")
 
-def _importance(grad: torch.Tensor, num_reserved, dist_type: str="abs"):
+def _importance(grad: torch.Tensor, num_reserved: int, dist_type: str="abs"):
     if dist_type == "gcc":
-        grad_vec = grad.view(grad.numel(), -1)
-        grad_norm = torch.norm(grad_vec, 2, 1, keepdim=True)
-        grad_mean = torch.mean(grad_norm)
-        return torch.cdist(grad_vec, grad_mean.view(1, 1)).view(-1)
-
-        """
-        _numel = grad.numel()
-        grad_vec = grad.view(_numel, -1)
-        similar = torch.zeros(_numel).cuda()
-        for idx, item in enumerate(grad_vec):
-            similar[idx] = torch.sum(torch.abs(torch.cdist(item.view(1, 1), grad_vec)))
-        return similar
-        """
-
-        """
-        grad_vec = grad.view(grad.numel(), -1)
-        # FIXME
-        grad_norm = torch.norm(grad_vec, 2, 1, keepdim=True) # type: ignore
-
-        # On CPU
-        # grad_norm_np = grad_norm.cpu().numpy()
-        # similar_matrix = distance.cdist(grad_norm_np, grad_norm_np, 'euclidean')
-        # similar_sum = np.sum(np.abs(similar_matrix), axis=0)
-        # return torch.from_numpy(similar_sum).to(grad.device)
-
-        # On GPU
-        similar_matrix = torch.cdist(grad_norm, grad_norm)
-        # FIXME
-        similar_sum = torch.sum(torch.abs(similar_matrix), axis=0) # type: ignore
-        return similar_sum
-        """
+        return _gcc(grad)
     elif dist_type == "l1":
-        grad_vec = grad.view(grad.numel(), -1)
-        grad_norm = torch.norm(grad_vec, 1, 1, keepdim=True)
-        grad_mean = torch.mean(grad_norm)
-        return torch.cdist(grad_vec, grad_mean.view(1, 1)).view(-1)
-
-        """
-        _numel = grad.numel()
-        grad_vec = grad.view(_numel, -1)
-        similar = torch.zeros(_numel).cuda()
-        for idx, item in enumerate(grad_vec):
-            similar[idx] = torch.sum(torch.abs(torch.cdist(item.view(1, 1), grad_vec)))
-        return similar
-        """
-
-        """
-        grad_vec = grad.view(-1, 1)
-        # FIXME
-        grad_norm = torch.norm(grad_vec, 1, 1, keepdim=True) # type: ignore
-
-        # # On CPU
-        # grad_norm_np = grad_norm.cpu().numpy()
-        # similar_matrix = 1 - distance.cdist(grad_norm_np, grad_norm_np, 'cosine')
-        # similar_sum = np.sum(np.abs(similar_matrix), axis=0)
-        # return torch.from_numpy(similar_sum).to(grad.device)
-
-        # On GPU
-        similar_matrix = torch.cdist(grad_norm, grad_norm)
-        # FIXME
-        similar_sum = torch.sum(torch.abs(similar_matrix), axis=0) # type: ignore
-        return similar_sum
-        """
+        return _l1(grad)
     elif dist_type == "abs":
-        grad_vec = grad.view(-1)
-        return torch.abs(grad_vec)
+        return _abs(grad)
     else:
         raise Exception("dist_type must be in [l2, cos, l1, abs]")
 
@@ -112,3 +51,76 @@ def _update(grad, indices, name, v):
 
     v[name] = v_vec.view(v[name].shape)
     return A.view(v[name].shape)
+
+def _base_line():
+    return
+
+def _gcc(grad: torch.Tensor):
+    grad_vec = grad.view(grad.numel(), -1)
+    grad_norm = torch.norm(grad_vec, 2, 1, keepdim=True)
+    grad_mean = torch.mean(grad_norm)
+    return torch.cdist(grad_vec, grad_mean.view(1, 1)).view(-1)
+    
+    """
+    _numel = grad.numel()
+    grad_vec = grad.view(_numel, -1)
+    similar = torch.zeros(_numel).cuda()
+    for idx, item in enumerate(grad_vec):
+        similar[idx] = torch.sum(torch.abs(torch.cdist(item.view(1, 1), grad_vec)))
+    return similar
+    """
+
+    """
+    grad_vec = grad.view(grad.numel(), -1)
+    # FIXME
+    grad_norm = torch.norm(grad_vec, 2, 1, keepdim=True) # type: ignore
+
+    # On CPU
+    # grad_norm_np = grad_norm.cpu().numpy()
+    # similar_matrix = distance.cdist(grad_norm_np, grad_norm_np, 'euclidean')
+    # similar_sum = np.sum(np.abs(similar_matrix), axis=0)
+    # return torch.from_numpy(similar_sum).to(grad.device)
+
+    # On GPU
+    similar_matrix = torch.cdist(grad_norm, grad_norm)
+    # FIXME
+    similar_sum = torch.sum(torch.abs(similar_matrix), axis=0) # type: ignore
+    return similar_sum
+    """
+
+def _l1(grad: torch.Tensor):
+    grad_vec = grad.view(grad.numel(), -1)
+    grad_norm = torch.norm(grad_vec, 1, 1, keepdim=True)
+    grad_mean = torch.mean(grad_norm)
+    return torch.cdist(grad_vec, grad_mean.view(1, 1)).view(-1)
+
+    """
+    _numel = grad.numel()
+    grad_vec = grad.view(_numel, -1)
+    similar = torch.zeros(_numel).cuda()
+    for idx, item in enumerate(grad_vec):
+        similar[idx] = torch.sum(torch.abs(torch.cdist(item.view(1, 1), grad_vec)))
+    return similar
+    """
+
+    """
+    grad_vec = grad.view(-1, 1)
+    # FIXME
+    grad_norm = torch.norm(grad_vec, 1, 1, keepdim=True) # type: ignore
+
+    # # On CPU
+    # grad_norm_np = grad_norm.cpu().numpy()
+    # similar_matrix = 1 - distance.cdist(grad_norm_np, grad_norm_np, 'cosine')
+    # similar_sum = np.sum(np.abs(similar_matrix), axis=0)
+    # return torch.from_numpy(similar_sum).to(grad.device)
+
+    # On GPU
+    similar_matrix = torch.cdist(grad_norm, grad_norm)
+    # FIXME
+    similar_sum = torch.sum(torch.abs(similar_matrix), axis=0) # type: ignore
+    return similar_sum
+    """
+
+def _abs(grad: torch.Tensor):
+    grad_vec = grad.view(-1)
+    return torch.abs(grad_vec)
