3f7eae9df121336d48596ca2e61b6224ee8fbe94
lutianen <tianen.xd@gmail.com>
2024-01-25 20:19:53 +0800

    Add 'wm5o' into path.

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,8 +8,9 @@ if not os.path.isdir(args.job_dir):
     os.makedirs(args.job_dir)
 
 args.prefix = time_file_str()
-args.job_dir = os.path.join(args.job_dir, "{}_{}_cr{}_wes{}".format(
-    args.cfg, args.dist_type, args.cr, args.warmup_epochs))
+args.job_dir = os.path.join(args.job_dir, "{}_{}_cr{}_wm{}{}".format(
+    args.cfg, args.dist_type, args.cr, args.warmup_epochs, 
+    "o" if args.warmup_epochs > 0 and len(args.warmup_coeff) > 0 else ""))
 if not os.path.isdir(args.job_dir):
     os.makedirs(args.job_dir)
 
@@ -28,8 +29,9 @@ def get_logger(file_path):
 
     return logger
 
-logger = get_logger(os.path.join(args.job_dir, "{}_{}_cr{}_wes{}_{}.log".format(
-    args.cfg, args.dist_type, args.cr, args.warmup_epochs, args.prefix)))
+logger = get_logger(os.path.join(args.job_dir, "{}_{}_cr{}_wm{}{}_{}.log".format(
+    args.cfg, args.dist_type, args.cr, args.warmup_epochs, 
+    "o" if args.warmup_epochs > 0 and len(args.warmup_coeff) > 0 else "", args.prefix)))
 
 def print_info(print_string, logger=logger):
     logger.info(print_string)
